eb7a53aa0c4679a51775848193ef387884edb979
Siddharth <siddharthlodha42@gmail.com>
2020-02-10 23:59:09 +0530

    first commiy

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import About from './components/About';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <About/>
+      </div>
+    );
+  }
+}
+
+export default App;
